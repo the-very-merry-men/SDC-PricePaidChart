16f706e3497d55333b71af741bf4680a690ba237
Chaynor Hsiao <chaynorhsiao@Chaynors-MacBook-Pro.local>
2019-05-17 19:15:19 -0700

    Implement seed script

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Faker from 'faker'
 
 class App extends Component {
   constructor(props) {
@@ -7,10 +8,29 @@ class App extends Component {
   render(props) {
     return (
       <div>
-        What up. 
+        I am a Price Paid Graph, or so I shall be, one day. 
       </div>
     )
   }
 }
 
+function generateUsers() {
+  let users = []
+  for (let id=1; id <= 100; id++) {
+
+    let firstName = Faker.name.firstName();
+    let lastName = Faker.name.lastName();
+    let email = Faker.internet.email();
+
+    users.push({
+        "id": id,
+        "first_name": firstName,
+        "last_name": lastName,
+        "email": email
+    });
+  }
+  return { "data": users }
+}
+let dataObj = generateUsers();
+console.log(dataObj);
 export default App
