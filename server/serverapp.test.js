dc4a658e7b66c4ff018d15dac77a79996353acfc
Chaynor Hsiao <chaynorhsiao@Chaynors-MacBook-Pro.local>
2019-05-21 12:36:01 -0700

    Implement mock graph with mock data

--- server/serverapp.test.js
+++ server/serverapp.test.js
@@ -1,2 +1,30 @@
-const server = require('./serverapp');
+const app = require('./serverapp');
+const request = require('supertest');
 
+describe('Test GET methods', () => {
+  test('Root path should respond with status code 200', (done) => {
+    request(app).get('/').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+  test('/api/stocks endpoint should respond with an array', (done) => {
+    request(app).get('/api/stocks').then((response) => {
+      expect(response.statusCode).toBe(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      done();
+    });
+  });
+  test('/api/stocks response should have necessary properties', (done) => {
+    request(app).get('/api/stocks').then((response) => {
+      expect(typeof response.body[0].id).toBe('number');
+      expect(typeof response.body[0].ticker).toBe('string');
+      expect(typeof response.body[0].current_price).toBe('number');
+      expect(typeof response.body[0].average_price).toBe('number');
+      expect(typeof response.body[0].week52high).toBe('number');
+      expect(typeof response.body[0].week52low).toBe('number');
+      done();
+    });
+  });
+  
+});
